15f7efbd6e695e73bd9668aad72a21f0b3f4bb98
Jim Andersson <andersson.jim@outlook.com>
2017-11-07 15:50:17 +0100

    Added GameManager

---
+++ Oneiro/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+    public static GameManager GetInstance()
+    {
+        return instance;
+    }
+
+    public enum State { PLAY, PAUSE }
+    private State state;
+
+    private void Awake()
+    {
+        instance = this;
+        SetState(State.PLAY);
+    }
+
+    public void SetState(State newState)
+    {
+        state = newState;
+    }
+}
